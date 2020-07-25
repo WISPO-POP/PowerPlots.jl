918ac6026dc1929b5c7b002dbc6bb2603fcb352d
Noah <noahx23@gmail.com>
2020-07-25 14:34:37 -0500

    Fix test set

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,24 @@ using PowerPlots
 using Test
 
 using PowerModels
+import Ipopt
 
 data = PowerModels.parse_file("$(joinpath(dirname(pathof(PowerModels)), ".."))/test/data/matpower/case5.m")
 
 @testset "PowerPlots.jl" begin
     # Write your own tests here.
-    @test graph = build_graph_network(case; exclude_sources=true)
-    @test plot_network(data)
-    @test plot_network!(data)
+     graph = build_graph_network(data; exclude_sources=true)
+     plot_network(data)
+     plot_network!(data)
 
-    @test plot_power_flow(data)
-    @test plot_network_status!(graph)
-    @test plot_system_voltage(data)
+
+     plot_network_status!(graph)
+     plot_system_voltage(data)
+
+     result = run_opf(data, DCPPowerModel, Ipopt.Optimizer)
+     PowerModels.update_data!(data, result["solution"])
+     plot_power_flow(data)
+
+     # these functions work if we got this far
+     @test true
 end
