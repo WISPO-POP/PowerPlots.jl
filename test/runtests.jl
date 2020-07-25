5b7b4cde7f1e25bebdbb661b0ece53a422eeb6a4
Noah <noahx23@gmail.com>
2020-07-25 14:02:29 -0500

    Add basic runtime tests.
    These tests are bad.  They are here to test that a function did not break in an update, but they are not sufficient unit tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,17 @@
 using PowerPlots
 using Test
 
+using PowerModels
+
+data = PowerModels.parse_file("$(joinpath(dirname(pathof(PowerModels)), ".."))/test/data/matpower/case5.m")
+
 @testset "PowerPlots.jl" begin
     # Write your own tests here.
-    @test PowerModelsGraph(2) != 0
+    @test graph = build_graph_network(case; exclude_sources=true)
+    @test plot_network(data)
+    @test plot_network!(data)
+
+    @test plot_power_flow(data)
+    @test plot_network_status!(graph)
+    @test plot_system_voltage(data)
 end
