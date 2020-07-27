3a45dcc425386f3bd65235c875e0899985a16966
noahrhodes <noahx23@gmail.com>
2020-07-27 11:13:23 -0500

    Fix: travisCI failed on failure to install scipy
    this call will import scypi into the install to be able to fun the networkx layout algorithm

--- src/PowerPlots.jl
+++ src/PowerPlots.jl
@@ -12,12 +12,17 @@ import Colors: @colorant_str
 import Plots
 
 # using NetworkLayout
+
 import PyCall
+
 const nx = PyCall.PyNULL()
+const scipy = PyCall.PyNULL()
 
 function __init__()
     copy!(nx, PyCall.pyimport_conda("networkx", "networkx"))
+    copy!(scipy, PyCall.pyimport_conda("scipy", "scipy"))
 end
+
 
 
 include("core/types.jl")  # must be first to properly define new types
