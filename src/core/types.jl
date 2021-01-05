d05ef6fac2809d7083c6262e8233538d0dc3802e
noahrhodes <noahx23@gmail.com>
2021-01-05 16:56:47 -0600

    Start new type for powermodels dataframe

--- src/core/types.jl
+++ src/core/types.jl
@@ -28,3 +28,31 @@ function PowerModelsGraph(nvertices::Int)
 
     return PowerModelsGraph{LightGraphs.SimpleDiGraph}(graph, metadata, annotationdata)
 end
+
+
+
+"""
+PowerModelsDataFrame{T<:LightGraphs.AbstractGraph}
+
+A structure containing a dataframe for each component type.
+"""
+mutable struct PowerModelsDataFrame
+    bus::DataFrame
+    gen::DataFrame
+    branch::DataFrame
+    dcline::DataFrame
+    load::DataFrame
+end
+
+
+"""
+    PowerModelsDataFrame(data::Dict{String,<:Any})
+
+Constructor for the PowerModelsDataFrame struct, given a PowerModels data dict.`
+"""
+function PowerModelsDataFrame(data::Dict{String,<:Any})
+
+
+
+    return PowerModelsDataFrame
+end
