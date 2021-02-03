73d2513b1a541d8fa202020c30e6eed1ceffd75a
Noah Rhodes <noahx23@gmail.com>
2021-02-03 12:32:33 -0600

    Add color attributes to plot

--- src/core/options.jl
+++ src/core/options.jl
@@ -1,7 +1,11 @@
 # Default plot attributes
 const default_plot_attributes = Dict{Symbol, Any}(
-  :nodecolor => :blue,
-  :edgecolor => :black
+  :gen_color => :blue,
+  :bus_color => :green,
+  :branch_color => :black,
+  :connector_color => :gray,
+  :dcline_color => :CadetBlue,
+  :storage_color => :steelblue,
 );
 
 # Returns a deepcopy of the default_plot_attributes, used to initialize plot_attributes in utils.jl
@@ -14,6 +18,10 @@ end
 # for example, nodecolor/markercolor/marker_color, edgecolor/edge_color/ec can be used to
 # set nodecolor and edgecolor respectively
 const attribute_aliases = Dict(
-  :nodecolor => [:marker_color, :markercolor],
-  :edgecolor => [:edge_color, :ec]
+  :gen_color => [:gencolor, :generatorcolor, :generator_color],
+  :bus_color => [:buscolor, :substationcolor, :substation_color],
+  :branch_color => [:branchcolor, :transmissionlinecolor, :transmissionline_color,:transmission_line_color],
+  :connector_color => [:connectorcolor],
+  :storage_color => [:storagecolor, :batterycolor, :battery_color],
+  :dcline_color => [:dclinecolor, :dc_line_color],
 )
