b178c7d4206e17015b1cc69cd4b8fc6a7791da67
Noah Rhodes <noahx23@gmail.com>
2021-05-05 14:21:58 -0500

    Move layout and attributres out of plot.jl

---
+++ src/core/attribute_validation.jl
@@ -0,0 +1,70 @@
+
+"Validates the given plot_attributes according to their type"
+function _validate_plot_attributes!(plot_attributes::Dict{Symbol, Any})
+    for attr in keys(plot_attributes)
+      if !haskey(default_plot_attributes, attr)
+        Memento.warn(_LOGGER, "Ignoring unexpected attribute $(repr(attr))")
+      end
+    end
+
+    # validate color attributes
+    for attr in _color_attributes
+        color = plot_attributes[attr]
+        if !(typeof(color) <: Union{String, Symbol, Vector})
+          Memento.warn(_LOGGER, "Color value for $(repr(attr)) should be given as symbol or string")
+        else
+          try
+            if typeof(color) <: Vector
+                parse.(Colors.Colorant, color) # parses all colors as CSS color
+            else
+                parse(Colors.Colorant, color) # try to parse the color as a CSS color
+                plot_attributes[attr] = [color] # package color into an array
+            end
+          catch e
+            Memento.warn(_LOGGER, "Invalid color $(repr(color)) given for $(repr(attr))")
+          end
+        end
+    end
+
+    # validate numeric attributes
+    for attr in _numeric_attributes
+      value = plot_attributes[attr]
+      if !(typeof(value) <: Union{Number, String})
+        Memento.warn(_LOGGER, "Value for $(repr(attr)) should be given as a number or numeric String")
+      elseif typeof(value) <: String
+        try
+            parse(Float64, value)
+        catch e
+            Memento.warn(_LOGGER, "Invalid number $(repr(value)) given for $(repr(attr))")
+        end
+      end
+    end
+
+    # validate data label attributes
+    for attr in _label_attributes
+      value = plot_attributes[attr]
+      if !(typeof(value) <: Union{String, Symbol})
+        Memento.warn(_LOGGER, "Value for $(repr(attr)) should be given as a String or Symbol")
+      end
+    end
+end
+
+
+"Checks that the given column plot_attributes[data_attr] exists in the data"
+function _validate_data(data::DataFrames.DataFrame, data_column::Any, data_name::String)
+    if !(typeof(data_column) <: Union{String, Symbol})
+        return
+    end
+    if !(data_column in names(data) || data_column in propertynames(data))
+        Memento.warn(_LOGGER, "Data column $(repr(data_column)) does not exist for $(data_name)")
+    end
+end
+
+"Checks that the given data type attribute is a valid VegaLite data type"
+function _validate_data_type(plot_attributes::Dict{Symbol, Any}, attr::Symbol)
+    valid_types = Set([:quantitative, :temporal, :ordinal, :nominal, :geojson])
+    data_type = plot_attributes[attr]
+    if !(Symbol(data_type) in valid_types)
+        Memento.warn(_LOGGER, "Data type $(repr(data_type)) not a valid VegaLite data type")
+    end
+end
--- src/layouts/common.jl
+++ src/layouts/common.jl
@@ -1,61 +1,153 @@
-# """
-#     layout_graph!(graph, layout_engine; kwargs...)
 
-# A routine to assign positions to all nodes of a `graph` for plotting using `layout_engine`.
-# Positions are assigned to the metadata of each node at `:x` and `:y`.
+"""
+Create a layout for a powermodels data dictionary.  This function creates a graph according to the specified keyword arguments `node_types`
+and `edge_types`.  A layout function is then applied, by default `layout_graph_KK!`.  A new case dictionary with the positions of the
+components is returned.
+"""
+function layout_graph_vega(case::Dict{String,<:Any};
+    spring_const=1e-2,
+    node_types::Array{String,1}=["bus","gen","storage"],
+    edge_types::Array{String,1}=["switch","branch","dcline","transformer"],
+    )
 
-# # Parameters
+    data = deepcopy(case)
+    node_comp_map = get_node_comp_map(data,node_types)
+    edge_comp_map = get_edge_comp_map(data,edge_types)
+    connector_map = get_connector_map(data,node_types)
 
-# * `graph::PowerModelsGraph`
+    G,ids = create_pm_graph(node_comp_map, edge_comp_map, connector_map)
+    positions = layout_graph_KK!(G, ids)
+    apply_node_positions!(data,positions, edge_comp_map, connector_map)
 
-#     Network graph
+    return data
+end
 
-# * `layout_engine`
 
-#     Default: `kamada_kawai_layout`. Layout Function to use. Applies only when not using `use_buscoords`.
+"Create a PowerModelsGraph using mappings for node->comp, edge->comp, connector->component"
+function create_pm_graph(node_comp_map::Dict{String,<:Any}, edge_comp_map::Dict{String,<:Any}, connector_map::Dict{String,<:Any})
+    G = PowerModelsGraph(0) # construct empty powermodels graph
+    ids = []
+    idmap = Dict()
+    i = 1 # set up iterator, need to associate LG generated indices with the 'id' field, can use metagraph to add 'id' field to
+    for (id, node) in node_comp_map
+        add_vertex!(G) # add vertex to graph
+        set_property!(G, i, :id, id) # set :id property to be equal to id.
+        push!(ids, id) # add node id (a string "compType_idNo") to list
+        push!(idmap, id => i) # push map from id to lg index to dictionary
+        i = i + 1 # increment i
+    end
 
-# * `use_buscoords::Bool`
+    for (id,edge) in edge_comp_map
+        add_edge!(G, idmap[edge["src"]], idmap[edge["dst"]])
+    end
 
-#     Default: `false`. If true, `spring_layout` will be used instead of `layout_engine`.
+    for (id,edge) in connector_map
+        add_edge!(G, idmap[edge["src"]], idmap[edge["dst"]])
+    end
+    return G,ids
+end
 
-# * `apply_spring_layout::Bool`
 
-#     Default: `false`. If true, `spring_layout` will be applied after `layout_engine` to ensure separation of overlapping nodes.
+"""
+    Create a dictionary map for nodes from components. This is used to create a PowerModelsGraph structure. `node_types` is a string
+    array for of all the components that are considered nodes in the graph.
+    ```Dict("gen_1"=>case["gen"]["1"])```
+"""
+function get_node_comp_map(data::Dict{String,<:Any},node_types::Array{String,1})
+    node_comp_map = Dict{String,Any}()
+    for node_type in node_types
+        temp_node = get(data, node_type, Dict{String,Any}())
+        temp_map = Dict(string(comp["source_id"][1], "_", comp["source_id"][2]) => comp  for (comp_id, comp) in temp_node)
+        merge!(node_comp_map, temp_map)
+    end
+    return node_comp_map
+end
 
-# * `spring_const::Float64`
 
-#     Default: `1e-3`. Spring constant to be used by `spring_layout`.
+"""
+    Create a dictionary map for edges from components. This is used to create a PowerModelsGraph structure. `edge_types` is a string
+    array for of all the components that are considered edges in the graph, like branches and dclines.
+    ```Dict("branch_1"=>case["branch"]["1"])```
+    A dictionary entry is added to specify the from and to buses in node mapping form `bus_1`.
+"""
+function get_edge_comp_map(data::Dict{String,<:Any},node_types::Array{String,1})
+    edge_comp_map = Dict{String,Any}()
+    for edge_type in edge_types
+        temp_edge = get(data, edge_type, Dict{String,Any}())
+        for (id,edge) in temp_edge
+            edge["src"] = "bus_$(edge["f_bus"])"
+            edge["dst"] = "bus_$(edge["t_bus"])"
+        end
+        temp_map = Dict(string(comp["source_id"][1],"_",comp["source_id"][2]) => comp for (comp_id, comp) in temp_edge)
+        merge!(edge_comp_map,temp_map)
+    end
+    return edge_comp_map
+end
 
-# * `kwargs`
 
-#     Keyword arguments to be used in `layout_engine`.
-# """
-# function layout_graph!(graph::PowerModelsGraph{T}, layout_engine=kamada_kawai_layout;
-#                        use_buscoords::Bool=false,
-#                        apply_spring_layout::Bool=false,
-#                        spring_const::Float64=1e-3,
-#                        kwargs...) where T <: LightGraphs.AbstractGraph
-#     if use_buscoords
-#         pos = Dict(node => get_property(graph, node, :buscoord, missing) for node in vertices(graph))
-#         fixed = [node for (node, p) in pos if !ismissing(p)]
+"""
+    Create a dictionary map for edges that connect nodes to buses. This is used to create a PowerModelsGraph structure. `node_types` is a string
+    array for of all the components that are considered nodes in the graph, and a connector branch is created for each non-bus node between the
+    node and the parent bus.
+    ```Dict("connector_1"=>Dict("src"=>"gen_1","dst"=>"bus_1"))```
+"""
+function get_connector_map(data::Dict{String,<:Any},node_types::Array{String,1})
+    connector_map = Dict{String,Any}()
+    for node_type in node_types
+        if node_type!="bus"
+            nodes = get(data, node_type, Dict{String,Any}())
+            for (id,node) in nodes
+                temp_connector = Dict{String,Any}()
+                temp_connector["src"] = "$(node_type)_$(id)"
+                temp_connector["dst"] = "bus_$(node["$(node_type)_bus"])"
 
-#         avg_x, avg_y = mean(hcat(skipmissing([v for v in values(pos)])...), dims=2)
-#         std_x, std_y = std(hcat(skipmissing([v for v in values(pos)])...), dims=2)
-#         for (v, p) in pos
-#             if ismissing(p)
-#                 pos[v] = get(pos,get_property(graph,v,:parent_node,missing),[avg_x,avg_y]) + [std_x*rand(), std_y*rand()]
-#             end
-#         end
-#         positions = spring_layout(graph; pos=pos, fixed=fixed, k=spring_const*minimum(std([p for p in values(pos)])), iterations=100)
-#     else
-#         positions = layout_engine(graph; kwargs...)
-#         if apply_spring_layout
-#             positions = spring_layout(graph; pos=positions, k=spring_const*minimum(std([p for p in values(positions)])), iterations=100)
-#         end
-#     end
+                temp_map = Dict(string("connector_",(length(connector_map) + 1)) => temp_connector)
+                merge!(connector_map,temp_map)
+            end
+        end
+    end
+    return connector_map
+end
 
-#     for (node, (x, y)) in positions
-#         set_property!(graph, node, :x, x)
-#         set_property!(graph, node, :y, y)
-#     end
-# end
+
+"Apply positions to the data, using the mapping in edge_comp_map and connector_map"
+function apply_node_positions!(data,positions, edge_comp_map, connector_map)
+    # Set Node Positions
+    for (node, (x, y)) in positions
+        (comp_type,comp_id) = split(node, "_")
+        data[comp_type][comp_id]["xcoord_1"] = x
+        data[comp_type][comp_id]["ycoord_1"] = y
+    end
+    # Set Edge positions
+    for (edge, val) in (edge_comp_map)
+        (x,y) = positions[val["src"]]
+        (x2,y2) = positions[val["dst"]]
+        (comp_type,comp_id) = split(edge, "_")
+        data[comp_type][comp_id]["xcoord_1"] = x
+        data[comp_type][comp_id]["ycoord_1"] = y
+        data[comp_type][comp_id]["xcoord_2"] = x2
+        data[comp_type][comp_id]["ycoord_2"] = y2
+    end
+
+    # Create connector dictionary
+    data["connector"] = Dict{String,Any}()
+    for (edge, con) in connector_map
+        _,id = split(edge, "_")
+        data["connector"][id] =  Dict{String,Any}(
+            "src" => con["src"],
+            "dst" => con["dst"],
+        )
+    end
+    # Set Connector positions
+    for (connector, val) in (connector_map)
+        (x,y) = positions[val["src"]]
+        (x2,y2) = positions[val["dst"]]
+        (comp_type,comp_id) = split(connector, "_")
+        data[comp_type][comp_id]["xcoord_1"] = x
+        data[comp_type][comp_id]["ycoord_1"] = y
+        data[comp_type][comp_id]["xcoord_2"] = x2
+        data[comp_type][comp_id]["ycoord_2"] = y2
+    end
+
+    return data
+end
