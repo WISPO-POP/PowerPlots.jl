e2fe0b73016f840a24df1c489fa1d1b8d1357ede
Noah Rhodes <nrhodes@wisc.edu>
2021-05-12 13:39:58 -0500

    Add cartesian2geo! to experimental

--- docs/src/experimental.md
+++ docs/src/experimental.md
@@ -8,3 +8,19 @@ julia> using PowerPlots.Experimental
 ```
 
 ## Apply geographic coordinates
+Change the coordinates from cartesian to a geographic projection. This is experimental because it is not well tested.
+VegaLite does not support geographic projections and zooming/panning yet, so combining with `add_zoom!` will not work.
+
+```@example
+using PowerPlots
+using PowerPlots.Experimental
+using PowerModels
+
+case = parse_file("case14.m")
+plot1 = powerplot(case; width=300, height=300)
+plot2 = deepcopy(plot1)
+PowerPlots.Experimental.cartesian2geo!(plot2)
+
+p = [plot1 plot2]
+p
+```
--- src/experimental/experimental.jl
+++ src/experimental/experimental.jl
@@ -1,6 +1,6 @@
 
 module Experimental
-    using VegaLite, ColorSchemes
+    using VegaLite, ColorSchemes, Setfield
 
     ## set branch color scheme after graph is created.  This does not currently function because the layer numbers are may
     # not correlate if a device does not exist (e.g. dclines)
@@ -46,6 +46,30 @@ module Experimental
 
     function set_gen_color_range(plot::VegaLite.VLSpec, cr::AbstractVector{String})
         return plot.layer[4]["encoding"]["color"]["scale"]["range"] = cr
+    end
+
+    "Change the cartesian view to a geographic projection. Default is albersUsa"
+    function cartesian2geo!(plot::VegaLite.VLSpec; projection_type=:albersUsa)
+
+        # set the plot geo map projection
+        @set! plot.projection=Dict{String,Any}("type"=>projection_type)
+
+        # create lat/lon channels from x/y channels
+        for i in 1:length(plot.layer)
+            plot.layer[i]["encoding"]["longitude"] = plot.layer[i]["encoding"]["x"]
+            delete!(plot.layer[i]["encoding"],"x")
+            plot.layer[i]["encoding"]["latitude"] = plot.layer[i]["encoding"]["y"]
+            delete!(plot.layer[i]["encoding"],"y")
+            if haskey(plot.layer[i]["encoding"],"x2")
+                plot.layer[i]["encoding"]["longitude2"] = plot.layer[i]["encoding"]["x2"]
+                delete!(plot.layer[i]["encoding"],"x2")
+            end
+            if haskey(plot.layer[i]["encoding"],"y2")
+                plot.layer[i]["encoding"]["latitude2"] = plot.layer[i]["encoding"]["y2"]
+                delete!(plot.layer[i]["encoding"],"y2")
+            end
+        end
+        return plot
     end
 
 end
