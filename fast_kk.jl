77c93f028c63fc1ba6bb35944e0f38d673f50265
Noah Rhodes <noahx23@gmail.com>
2021-07-26 11:17:05 -0500

    Add profile view

--- fast_kk.jl
+++ fast_kk.jl
@@ -11,11 +11,13 @@ using OMEinsum
 path = "C:/Users/noahx/Documents/PowerDev/pglib-opf/"
 PowerModels.silence()
 
-for case in ["pglib_opf_case14_ieee.m","pglib_opf_case118_ieee.m","pglib_opf_case500_tamu.m","pglib_opf_case1354_pegase.m"]
+for case in ["pglib_opf_case14_ieee.m","pglib_opf_case118_ieee.m"]#,"pglib_opf_case500_tamu.m","pglib_opf_case1354_pegase.m"]
     println(case)
     pmg = PowerModelsGraph(PowerModels.parse_file("$(path)$(case)"))
-    @time pos1 = PowerPlots.kamada_kawai(pmg)
-    @time pos2 = PowerPlots.new_kamada_kawai(pmg)
+    ProfileView.@profview pos1 = PowerPlots.kamada_kawai(pmg)
+    ProfileView.@profview pos2 = PowerPlots.new_kamada_kawai(pmg)
+    # @time pos1 = PowerPlots.kamada_kawai(pmg)
+    # @time pos2 = PowerPlots.new_kamada_kawai(pmg)
     println("Same Layout: $(all(pos1 .≈  pos2))") # same result?
     println()
 end
