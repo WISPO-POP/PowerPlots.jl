c1a3e76fb671d5e1e943508034e8a6d6c6846d81
noahrhodes <nrhodes@wisc.edu>
2021-11-05 13:48:04 -0500

    Faster KamadaKawaii gradient (#74)
    Optimize KamadaKawaii function for better speed.

--- src/layouts/layout_engines.jl
+++ src/layouts/layout_engines.jl
@@ -1,6 +1,15 @@
 
+"""
+    `layout_graph_kamada_kawai!(G::PowerModelsGraph; kwargs...)`
+    Apply the `kamada_kawai` layout to a PowerModelsGraph.
+"""
+function layout_graph_kamada_kawai!(G::PowerModelsGraph; kwargs...)
+    return positions = _kamada_kawai(G, kwargs...)
+end
+
+
 "Kamada Kawai Layout"
-function kamada_kawai(G::PowerModelsGraph, dist::Union{Nothing,Matrix{Float64}}=nothing, pos::Union{Nothing,Matrix{Float64}}=nothing, weight="weight", scale=1, center=nothing, dim=2)
+function _kamada_kawai(G::PowerModelsGraph, dist::Union{Nothing,AbstractMatrix{<:Real}}=nothing, pos::Union{Nothing,AbstractMatrix{<:Real}}=nothing, weight="weight", scale=1, center=nothing, dim=2)
     graph = G.graph # convert to undirected graph
     nNodes = LightGraphs.nv(graph)
     if nNodes == 0
@@ -13,7 +22,7 @@ function kamada_kawai(G::PowerModelsGraph, dist::Union{Nothing,Matrix{Float64}}=
             dist[i]=LightGraphs.dijkstra_shortest_paths(graph, i).dists
         end
     end
-    dist_mtx = 1e6 * ones(nNodes, nNodes) #reform dist into matrix, probably easier??
+    dist_mtx = 1e6 * ones(nNodes, nNodes)
     for nr in LightGraphs.vertices(graph)
         rdist = dist[nr]
         for nc in LightGraphs.vertices(graph)
@@ -22,7 +31,7 @@ function kamada_kawai(G::PowerModelsGraph, dist::Union{Nothing,Matrix{Float64}}=
     end
     if pos===nothing
         if dim >= 3
-            pos= 2 .* rand(Float64,dim, nNodes) .- 1 #??? make a matrix n x dim
+            pos= 2 .* rand(Float64,dim, nNodes) .- 1
         elseif dim == 2
             a = LightGraphs.adjacency_matrix(graph)
             pos = convert(Array,RecursiveArrayTools.VectorOfArray(NetworkLayout.Circular.layout(a)))
@@ -42,46 +51,75 @@ Anneal node locations based on the Kamada-Kawai cost-function,
 using the supplied matrix of preferred inter-node distances,
 and starting locations.
 """
-function _kamada_kawai_solve(dist_mtx::Array{Float64,2}, pos_arr::Array{Float64,2}, dim::Int, nNodes::Int)::Matrix{Float64}
+function _kamada_kawai_solve(dist_mtx::AbstractMatrix{<:Real}, pos_arr::AbstractMatrix{<:Real}, dim::Int, nNodes::Int)#<:AbstractMatrix{<:Real}
     pos_vec = pos_arr[:]
     meanwt = 1e-3
+    invdist = 1.0./(dist_mtx + LinearAlgebra.I(nNodes) * 1e-3)
+
+    delta = Array{eltype(pos_arr),3}(undef,dim,nNodes,nNodes)
+    nodesep = similar(dist_mtx)
+    inv_nodesep = similar(dist_mtx)
+    parr = similar(pos_arr)
+    direction = Array{eltype(pos_arr),3}(undef,dim,nNodes,nNodes)
+    offset = similar(dist_mtx)
+    gradient = similar(pos_arr)
+    g1 = similar(pos_arr)
+    g2 = similar(pos_arr)
+
 
     opt = NLopt.Opt(:LD_LBFGS, length(pos_vec))
     opt.xtol_rel = 1e-4
-    opt.min_objective = (pos_vec, grad)->_kamada_kawai_costfn(pos_vec,grad,1.0./(dist_mtx + LinearAlgebra.I(nNodes) * 1e-3), meanwt, dim, nNodes)
+    opt.min_objective = (pos_vec, grad)->_kamada_kawai_costfn(pos_vec,grad,nodesep,inv_nodesep,parr,gradient,g1,g2,invdist,delta,direction,offset, meanwt, dim, nNodes)
     (minf,minx,ret) = NLopt.optimize(opt, pos_vec)
 
     numevals = opt.numevals # the number of function evaluations
-    #println("got $minf at $minx after $numevals iterations (returned $ret)")
+    # println("got $minf at $minx after $numevals iterations (returned $ret)")
+    # println("got $minf after $numevals iterations (returned $ret)")
     return reshape(minx, dim, nNodes)
 end
 
 "Cost-function and gradient for Kamada-Kawai layout algorithm"
-function _kamada_kawai_costfn(pos_vec::Vector{Float64}, grad::Vector{Float64}, invdist::Array{Float64,2}, meanweight::Float64, dim::Int, nNodes::Int)
-    pos_arr = reshape(pos_vec,dim,nNodes)
-    delta = zeros(Float64,dim,nNodes,nNodes)
+function _kamada_kawai_costfn(
+    pos_vec::AbstractVector{<:Real},
+    grad::AbstractVector{<:Real},
+    nodesep::AbstractMatrix{<:Real},
+    inv_nodesep::AbstractMatrix{<:Real},
+    pos_arr::AbstractMatrix{<:Real},
+    gradient::AbstractMatrix{<:Real},
+    g1::AbstractMatrix{<:Real},
+    g2::AbstractMatrix{<:Real},
+    invdist::AbstractMatrix{<:Real},
+    delta::AbstractArray{<:Real,3},
+    direction::AbstractArray{<:Real,3},
+    offset::AbstractMatrix{<:Real},
+    meanweight::Real,
+    dim::Int,
+    nNodes::Int
+    )::Real
+
+    pos_arr .= reshape(pos_vec,dim,nNodes)
     for i in 1:nNodes
-        delta[:,:,i] = pos_arr .- pos_arr[:,i]
+        delta[:,:,i] .= pos_arr .- pos_arr[:,i]
     end
 
-    nodesep = zeros(Float64, dim, nNodes)
-    nodesep =  reshape(sqrt.(sum(x -> x^2, delta; dims=1)),nNodes,nNodes)
-    direction = OMEinsum.ein"ijk,jk -> ijk"(delta, 1.0./(nodesep+LinearAlgebra.I(nNodes)*1e-3))
+    nodesep .=  sqrt.(reshape(sum(x -> x^2, delta; dims=1),nNodes,nNodes)) # 1/3 allocations
+    inv_nodesep .= 1.0./(nodesep+LinearAlgebra.I(nNodes)*1e-3) # 1/3 allocations
+    _calc_direction!(delta,inv_nodesep,direction)
 
-    offset = nodesep .* invdist .- 1.0
+    offset .= nodesep .* invdist .- 1.0
     for i in 1:nNodes
         offset[i,i] = 0.0
     end
 
-    cost = 0.5 * sum(offset.^2)
-    gradient = OMEinsum.ein"jk,jk,ijk->ij"(invdist, offset, direction) - OMEinsum.ein"jk,jk,ijk->ik"(invdist, offset, direction)
+    cost = 0.5 * sum(x -> x^2, offset)
+    _calc_gradient!(invdist, offset, direction, g1,g2,gradient)
 
-    # # Additional parabolic term to encourage mean position to be near origin:
+    # Additional parabolic term to encourage mean position to be near origin:
     sumpos = sum(pos_arr, dims=2)
     cost += 0.5 .* meanweight .* sum(sumpos.^2)
     origin_penalty = meanweight*sumpos
-    for i in 1:nNodes
-        gradient[:,i] += origin_penalty
+    @inbounds for i in 1:nNodes
+        gradient[:,i] = gradient[:,i] .+ origin_penalty
     end
     if length(grad) > 0
         grad[:] = gradient[:]
@@ -90,7 +128,29 @@ function _kamada_kawai_costfn(pos_vec::Vector{Float64}, grad::Vector{Float64}, i
     return cost
 end
 
-function layout_graph_kamada_kawai!(G) #return type must be dictionary
-    return positions = kamada_kawai(G)
+function _calc_direction!(x::AbstractArray{<:Real, 3},y::AbstractArray{<:Real, 2}, z::AbstractArray{<:Real,3})::AbstractArray{<:Real,3}
+    @simd for i in axes(z,1)
+        @views z[i,:,:] = x[i,:,:].*y[:,:]
+    end
+    return z
 end
 
+
+function _calc_gradient!(
+    u::AbstractArray{<:Real, 2},v::AbstractArray{<:Real, 2}, w::AbstractArray{<:Real,3},
+    z1::AbstractArray{<:Real, 2},z2::AbstractArray{<:Real, 2},z::AbstractArray{<:Real,2}
+    )
+    z1 .= zero(eltype(z1))
+    z2 .= zero(eltype(z2))
+
+    @simd for i in axes(z,1)
+        for j in axes(z,2)
+             for k in axes(z,2)
+                z1[i,j] += u[j,k]*v[j,k]*w[i,j,k]
+                z2[i,k] += u[j,k]*v[j,k]*w[i,j,k]
+            end
+        end
+    end
+    z .= z1.-z2
+    return z
+end
