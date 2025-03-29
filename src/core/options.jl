87b0311ee7d95503c93508079863e4ba318c84d0
Noah Rhodes <nrhodes@wisc.edu>
2025-03-29 10:48:52 -0600

    Add new color schemes

--- src/core/options.jl
+++ src/core/options.jl
@@ -1,14 +1,42 @@
 # Default Color Schemes
-# catergory 20b and 20c used as reference
-const color_schemes=Dict{Symbol,Any}(
-    :blues => colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#3182BD", Colors.colorant"#C6DBEF", length=5))),#["#$(Colors.hex(c))" for c in ColorSchemes.ColorScheme(range(Colors.colorant"#3182BD", Colors.colorant"#C6DBEF", length=5))],
-    :greens => colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#31A354", Colors.colorant"#C7E9C0", length=5))),
-    :oranges => colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#E6550D", Colors.colorant"#FDD0A2", length=5))),
-    :reds => colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#CB181D", Colors.colorant"#FCBBA1", length=5))),
-    :purples => colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#756BB1", Colors.colorant"#DADAEB", length=5))),
-    :grays => colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#555555", Colors.colorant"#FFFFFF", length=5))),
-    :yellows => colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#8C6D31", Colors.colorant"#E7CB94", length=5))),
+color_schemes=Dict{Symbol,Any}(
+  :blues =>   colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#3182BD", Colors.colorant"#C6DBEF", length=5))),#["#$(Colors.hex(c))" for c in ColorSchemes.ColorScheme(range(Colors.colorant"#3182BD", Colors.colorant"#C6DBEF", length=5))],
+  :greens =>  colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#31A354", Colors.colorant"#C7E9C0", length=5))),
+  :oranges => colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#E6550D", Colors.colorant"#FDD0A2", length=5))),
+  :reds =>    colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#CB181D", Colors.colorant"#FCBBA1", length=5))),
+  :purples => colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#756BB1", Colors.colorant"#DADAEB", length=5))),
+  :grays =>   colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#555555", Colors.colorant"#FFFFFF", length=5))),
+  :browns =>  colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#8C6D31", Colors.colorant"#E7CB94", length=5))),
+  :pinks =>   colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#e7298a", Colors.colorant"#fbb3cc", length=5))),
+  :yellows => colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#ffc108", Colors.colorant"#ffef79", length=5))),
+  :teal =>    colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#2a8482", Colors.colorant"#46e1d1", length=5))),
+  :lime =>    colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#8df33c", Colors.colorant"#c8ff87", length=5))),
+  :violet =>  colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#8f3f8f", Colors.colorant"#d6a0d6", length=5))),
+  :cyan =>    colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#00ffff", Colors.colorant"#99ffff", length=5))),
+  :magenta => colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#ff00ff", Colors.colorant"#ff99ff", length=5))),
+  :indigo =>  colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#4b0082", Colors.colorant"#9b59b6", length=5))),
+  :cherry =>  colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#ff0000", Colors.colorant"#ff9999", length=5))),
 )
+
+component_color_order = Dict{Int,Symbol}(
+  1 => :blues,
+  2 => :greens,
+  3 => :oranges,
+  4 => :reds,
+  5 => :purples,
+  6 => :browns,
+  7 => :pinks,
+  8 => :yellows,
+  9 => :lime,
+  10 => :violet,
+  11 => :cyan,
+  12 => :magenta,
+  13 => :indigo,
+  14 => :cherry,
+  15 => :teal,
+  16 => :grays,
+)
+
 
 default_plot_attributes = Dict{Symbol, Any}(
   :width => 500,
@@ -17,7 +45,7 @@ default_plot_attributes = Dict{Symbol, Any}(
 )
 
 default_edge_attributes = Dict{Symbol, Any}(
-  :color => color_schemes[:blues],
+  # :color => color_schemes[:blues],
   :size => 5,
   :data => "ComponentType",
   :data_type => "nominal",
@@ -36,7 +64,7 @@ default_connector_attributes = Dict{Symbol, Any}(
 )
 
 default_node_attributes = Dict{Symbol, Any}(
-  :color => color_schemes[:greens],
+  # :color => color_schemes[:greens],
   :size => 5e2,
   :data => "ComponentType",
   :data_type => "nominal",
