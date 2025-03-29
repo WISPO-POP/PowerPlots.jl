ed2f85cde2f8e7b09c4be0c7be1f4e1e811d36fe
Noah Rhodes <nrhodes@wisc.edu>
2025-03-29 17:03:42 -0600

    Update documenter html size limit

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,7 @@
 using Documenter, PowerPlots, VegaLite, UUIDs, PowerModels, PowerModelsDistribution
 
 function Base.show(io::IO, m::MIME"text/html", v::VegaLite.VLSpec)
-    divid = string("vl", replace(string(uuid4()), "-"=>""))
+    divid = string("vl", replace(string(uuid4()), "-" => ""))
     print(io, "<div id='$divid' style=\"width:100%;height:100%;\"></div>")
     print(io, "<script type='text/javascript'>requirejs.config({paths:{'vg-embed': 'https://cdn.jsdelivr.net/npm/vega-embed@6?noext','vega-lib': 'https://cdn.jsdelivr.net/npm/vega-lib?noext','vega-lite': 'https://cdn.jsdelivr.net/npm/vega-lite@4?noext','vega': 'https://cdn.jsdelivr.net/npm/vega@5?noext'}}); require(['vg-embed'],function(vegaEmbed){vegaEmbed('#$divid',")
     VegaLite.our_json_print(io, v)
@@ -11,7 +11,11 @@ end
 makedocs(
     warnonly = true,
     modules = [PowerPlots],
-    format = Documenter.HTML(mathengine = Documenter.MathJax()),
+    format = Documenter.HTML(mathengine = Documenter.MathJax(),
+        size_threshold_warn = 2^20,
+        size_threshold = nothing,
+        example_size_threshold = nothing,
+    ),
     sitename = "PowerPlots",
     authors = "Noah Rhodes",
     pages = [
