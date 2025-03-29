cca9a4347a86ec0d97928733ba47fe8453e9b9cc
Noah Rhodes <nrhodes@wisc.edu>
2025-03-29 11:12:25 -0600

    Add scratchspace work

--- test/scratchspace.jl
+++ test/scratchspace.jl
@@ -68,4 +68,44 @@ powerplot(eng)
 # node names, (s,d) names, etc?
 # now the parallel edges doesn't work for transformers...
 
+
+
+using Plots
+test_color_schemes=Dict{Symbol,Any}(
+    :blues =>   colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#3182BD", Colors.colorant"#C6DBEF", length=5))),#["#$(Colors.hex(c))" for c in ColorSchemes.ColorScheme(range(Colors.colorant"#3182BD", Colors.colorant"#C6DBEF", length=5))],
+    :greens =>  colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#31A354", Colors.colorant"#C7E9C0", length=5))),
+    :oranges => colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#E6550D", Colors.colorant"#FDD0A2", length=5))),
+    :reds =>    colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#CB181D", Colors.colorant"#FCBBA1", length=5))),
+    :purples => colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#756BB1", Colors.colorant"#DADAEB", length=5))),
+    :grays =>   colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#555555", Colors.colorant"#FFFFFF", length=5))),
+    :browns =>  colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#8C6D31", Colors.colorant"#E7CB94", length=5))),
+    :pinks =>   colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#e7298a", Colors.colorant"#fbb3cc", length=5))),
+    :yellows => colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#ffc108", Colors.colorant"#ffef79", length=5))),
+    :teal =>    colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#2a8482", Colors.colorant"#46e1d1", length=5))),
+    :lime =>    colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#8df33c", Colors.colorant"#c8ff87", length=5))),
+    :violet =>  colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#8f3f8f", Colors.colorant"#d6a0d6", length=5))),
+    :cyan =>    colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#00ffff", Colors.colorant"#99ffff", length=5))),
+    :magenta => colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#ff00ff", Colors.colorant"#ff99ff", length=5))),
+    :indigo =>  colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#4b0082", Colors.colorant"#9b59b6", length=5))),
+    :cherry =>  colorscheme2array(ColorSchemes.ColorScheme(range(Colors.colorant"#ff0000", Colors.colorant"#ff9999", length=5))),
+)
+
+
+# investigate new color schemes
+# using ColorSchemes, Colors
+# ColorSchemes.ColorScheme([parse(Colorant, x) for x in test_color_schemes[:pinks]])
+# ColorSchemes.ColorScheme([parse(Colorant, x) for x in test_color_schemes[:blues]])
+
+
+begin
+  p=plot()
+  for (i,k) in enumerate(keys(test_color_schemes))
+    scatter!(1:5, ones(5)*i, color=test_color_schemes[k], label="$k", ms=30, legend=false)
+
+  end
+  xlims!(0,6)
+  ylims!(0,length(test_color_schemes)+1)
+  p
+end
+
 ################################################
